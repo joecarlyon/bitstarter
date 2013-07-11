8f0539773acd12d466cb85175d14993e1ec872d9
Joe Carlyon <joecarlyon@gmail.com>
2013-07-11 05:52:40 +0000

    Changed index.html to only contain a string and web.js to still use app.get in it and not in a buffer

--- web.js
+++ web.js
@@ -2,7 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var buf = fs.readFileSync('index.html');
-response.send(buf.toString());
+
+app.get('/', function(request, response) {
+  //response.send('Hello World 2!');
+  response.send(buf.toString());
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
