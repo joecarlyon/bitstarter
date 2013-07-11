af12e98a5f0c1c46224145d23be015165e53937b
Joe Carlyon <joecarlyon@gmail.com>
2013-07-11 05:45:47 +0000

    Got rid of unneccessary lines

--- web.js
+++ web.js
@@ -1,12 +1,8 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-buf = fs.readFileSync('index.html');
-
-app.get('/', function(request, response) {
-  //response.send('Hello World 2!');
-  buf.toString();
-});
+var buf = fs.readFileSync('index.html');
+response.send(buf.toString());
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
